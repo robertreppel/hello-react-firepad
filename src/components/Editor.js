2252020139b7b3b2d9145fbb7401e929015d3b1f
Robert Reppel <robert.reppel@adaptechsolutions.ca>
2017-12-21 09:51:55 -0800

    Better cohesion.

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -2,6 +2,45 @@ import React, { Component } from "react";
 import './Editor.css'; 
 
 class Editor extends Component {
+  componentDidMount() {
+    //// Initialize Firebase.
+    //// TODO: replace with your Firebase project configuration.
+    var config = {
+      apiKey: "AIzaSyC_sS0XzK92EuGnt-i2CuNNhwH57ag8sJA",
+      databaseURL: "https://hello-firebase-rr.firebaseio.com",
+    };
+    window.firebase.initializeApp(config);
+    //// Get Firebase Database reference.
+    var firepadRef = this.getExampleRef();
+    //// Create CodeMirror (with lineWrapping on).
+    var codeMirror = window.CodeMirror(document.getElementById('firepad-container'), { lineWrapping: true });
+    //// Create Firepad (with rich text toolbar and shortcuts enabled).
+    var firepad = window.Firepad.fromCodeMirror(firepadRef, codeMirror,
+        { richTextToolbar: true, richTextShortcuts: true });
+    //// Initialize contents.
+    firepad.on('ready', function() {
+      if (firepad.isHistoryEmpty()) {
+        firepad.setHtml('<span style="font-size: 24px;">Rich-text editing with <span style="color: red">Firepad!</span></span><br/><br/>Collaborative-editing made easy.\n');
+      }
+    });
+  }
+
+  // Helper to get hash from end of URL or generate a random one.
+  getExampleRef() {
+    var ref = window.firebase.database().ref();
+    var hash = window.location.hash.replace(/#/g, '');
+    if (hash) {
+      ref = ref.child(hash);
+    } else {
+      ref = ref.push(); // generate unique location.
+      window.location = window.location + '#' + ref.key; // add it as a hash to the URL.
+    }
+    if (typeof console !== 'undefined') {
+      console.log('Firebase data: ', ref.toString());
+    }
+    return ref;
+  }
+
   render() {
     return (
         <div>
